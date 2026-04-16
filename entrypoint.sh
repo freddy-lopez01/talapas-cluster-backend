5a92512fff8e8cd62e5e3f617a10c0070f32208a
Freddy Lopez <freddlopez01@gmail.com>
2026-04-16 13:51:52 -0700

    entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-# Start cpu_usage.py in the background
-python /app/cpu_usage.py &
-python /app/scontrol_scaper.py &
-
 
 # Start the Flask app in the foreground (keeps container alive)
 python /app/cpu_receiver.py
